cfa2b57f486cbd4fa8b705293e66d6664b41c425
edperesez <edperesez7@hotmai.com>
2024-06-21 15:13:55 -0500

    configuracion plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 /* --------------------------------- import --------------------------------- */
 import express from 'express'
+import morgan from 'morgan';
+
+import {join,dirname} from 'path'
+import { fileURLToPath } from 'url';
+import { engine } from 'express-handlebars';
+
 /* ----------------------------- initialization ----------------------------- */
 const app = express();  
-/* --------------------------------- setting -------------------------------- */
-app.set('port', process.env.PORT || 3000 );
-/* ------------------------------- Middlewares ------------------------------ */ 
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+/* --------------------------------- setting -------------------------------- */
+app.set('port', process.env.PORT || 3000 ); 
+
+app.set('views', join(__dirname, 'views')); 
+app.engine('.hbs', engine({
+    defaultLayout: 'main', 
+    layoutsDir: join(app.get('views'), 'layouts'),
+    partialsDir: join(app.get('views'), 'partials'),
+    extname: '.hbs' 
+})); 
+app.set('view engine', '.hbs');
+/* ------------------------------- Middlewares ------------------------------ */ 
+app.use(morgan('dev')); 
+app.use(express.urlencoded({ extended: false})); 
+app.use(express.json());
 
 /* --------------------------------- routes --------------------------------- */
-
+app.get('/', (req, res) => {
+    res.json({"message": "hola mundo"})
+});
 /* ------------------------------ public files ------------------------------ */ 
+app.use(express.static(join(__dirname, 'public')))
 
 /* ------------------------------- Run server ------------------------------- */
-app.listen(app.get('port')), () => {
+app.listen(app.get('port'), () => {
     console.log('server listening on port', app.get('port'));
 
-};     
+});      
